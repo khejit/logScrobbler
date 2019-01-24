36d4a720c12b6df5c6fd6c1255fb50683077245b
khejit <mikolajsliwinski353@gmail.com>
2019-01-24 16:47:48 +0100

    Deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/logScrobbler/'
+        : '/',
     runtimeCompiler: true,
     configureWebpack: {
         output: {
