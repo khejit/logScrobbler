13d8edae23822c1275dede6e59848312c3de43ce
khejit <mikolajsliwinski353@gmail.com>
2020-03-10 21:14:55 +0100

    Loading set to false if incorrect token is sent to lastfm

--- src/components/auth/StartAuth.js
+++ src/components/auth/StartAuth.js
@@ -1,43 +1,44 @@
 import Auth from './Auth';
 
 export default {
-    mixins: [Auth],
-    data: function () {
-        return {
-            token: '',
-            loading: true
-        }
-    },
-    mounted: function () {
-        if (this.getTokenIfPresent()){
-            this.setUpLfm();
-            this.setNewSession();
-        } else {
-            this.loading = false;
-        }
-    },
-    methods: {
-        getTokenIfPresent: function () {
-            // const token = this.$route.query.token; // doesn't work in router hash mode
-            const urlParams = new URLSearchParams(window.location.search);
-            const token = urlParams.get('token');
-            this.token = token || '';
-            return !!this.token;
-        },
-        setNewSession: function () {
-            const that = this;
-            this.lfm.authenticate(this.token, function (err, session) {
-                if (err) {
-                    throw err;
-                }
-                let newSession = {};
-                ({ username: newSession.username, key: newSession.userkey } = session);
-                localStorage.setItem('session', JSON.stringify(newSession));
-                that.goToLogged()
-            });
-        },
-        goToLogged: function() {
-            this.$router.push({ name: 'logged' });
-        }
-    }
-}
+	mixins: [Auth],
+	data: function() {
+		return {
+			token: '',
+			loading: true,
+		};
+	},
+	mounted: function() {
+		if (this.getTokenIfPresent()) {
+			this.setUpLfm();
+			this.setNewSession();
+		} else {
+			this.loading = false;
+		}
+	},
+	methods: {
+		getTokenIfPresent: function() {
+			// const token = this.$route.query.token; // doesn't work in router hash mode
+			const urlParams = new URLSearchParams(window.location.search);
+			const token = urlParams.get('token');
+			this.token = token || '';
+			return !!this.token;
+		},
+		setNewSession: function() {
+			const that = this;
+			this.lfm.authenticate(this.token, function(err, session) {
+				if (err) {
+					that.loading = false;
+					throw err;
+				}
+				let newSession = {};
+				({ username: newSession.username, key: newSession.userkey } = session);
+				localStorage.setItem('session', JSON.stringify(newSession));
+				that.goToLogged();
+			});
+		},
+		goToLogged: function() {
+			this.$router.push({ name: 'logged' });
+		},
+	},
+};
